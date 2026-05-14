d68d239e4b5ddfc94186e6601d280149904df4cb
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-05-14 15:11:01 +0700

    implementasi tranversal di peek

--- stack.cpp
+++ stack.cpp
@@ -49,7 +49,24 @@ class stack
 
     void peek()
     {
-        
-        
+        if (top == NULL)
+        {
+            cout << "List is empty." << endl;
+        }
+         else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
+
+       bool isEmpty()
+    {
+        return top == NULL; 
     }
 };
